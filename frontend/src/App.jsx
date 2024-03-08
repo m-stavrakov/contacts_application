bce550c388a2831cd40bea0d07d80223e892e9ab
m-stavrakov <131870370+m-stavrakov@users.noreply.github.com>
2024-03-08 15:09:06 +0000

    Second commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import { useEffect } from 'react'
+import { useState } from 'react'
+import ContactList from './ContactList'
+import './App.css'
+import ContactForm from './ContactForm'
+
+function App() {
+  const [contacts, setContacts] = useState([])
+
+  useEffect(() => {
+    fetchContacts()
+  }, [])
+
+  const fetchContacts = async () => {
+    const response = await fetch("http://127.0.0.1:5000/contacts")
+    const data = await response.json()
+    setContacts(data.contacts)
+    console.log(data.contacts)
+  }
+  return <>
+    <ContactList contacts={contacts}/>
+    <ContactForm />
+  </>
+  
+}
+
+export default App
